55dc5841ddc609575c0e63e5c6881c9cd943c14b
Adam-92 <adam.kalcas2@gmail.com>
2021-08-24 15:33:27 +0200

    transition shoe

--- src/components/Shoe.js
+++ src/components/Shoe.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHandPointUp } from '@fortawesome/free-solid-svg-icons';
 
 const Shoe = ( {id,modelName,price,availableSize,img}) =>{
+    //Show icon while on MouseMove 
+    const [showPickIcon,setShowPickIcon] = useState(false);
+    console.log(showPickIcon);
     return(
-        <div className="container-shoe">
+        <div className="container-shoe" onMouseOver={()=>{setShowPickIcon(true)}}>
+            
             <div className="img-shoe">
                 <img src={img}></img>
             </div>
@@ -15,6 +21,9 @@ const Shoe = ( {id,modelName,price,availableSize,img}) =>{
                 <p>Size: {availableSize}</p>
                 <p>Price: {price}$</p>
             </div>
+            <div className="animation-shoe">
+                <FontAwesomeIcon icon={faHandPointUp} />
+            </div> 
         </div>
     )
 }
