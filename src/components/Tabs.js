ab8eb66c69e43f5ccb4c7c8edd43c39e425b9115
Adam-92 <adam.kalcas2@gmail.com>
2021-10-21 09:51:59 +0200

    change Tabs position

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -3,15 +3,17 @@ import {Link} from 'react-router-dom'
 //Tabs component Register/Login
 const Tabs = ( { tab }, ref ) => {
     return(
-        <div className="container-Tabs" ref={ref} >
-            {/*add class toggle to change the position of the lighted up tab*/}
-            <div className={`btn-color-Tabs ${tab === 2 ? 'toggle-Tabs' : ''}`}></div>
-            <Link to="/register" className="btn-Tabs">
-                Register
-            </Link>
-            <Link to="/login" className="btn-Tabs">
-                Login
-            </Link>
+        <div className="container-Tabs">
+            <div className="Tabs" ref={ref} >
+                {/*add class toggle to change the position of the lighted up tab*/}
+                <div className={`btn-color-Tabs ${tab === 2 ? 'toggle-Tabs' : ''}`}></div>
+                <Link to="/register" className="btn-Tabs">
+                    Register
+                </Link>
+                <Link to="/login" className="btn-Tabs">
+                    Login
+                </Link>
+            </div>
         </div>
     )
 }
